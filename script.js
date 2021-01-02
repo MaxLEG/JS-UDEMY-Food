206545b158d0df7911f4204227ed5ed424061828
Max <soldier322@gmail.com>
2021-01-02 21:41:33 +0100

    debut film project

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-"use strict";
-a = 15
-console.log(a)
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt("Один из  последних просмотренных фильмов?", ""),
+    b = +prompt("На сколько оцените его?", ""),
+    c = prompt("Один из  последних просмотренных фильмов?", ""),
+    d = +prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
