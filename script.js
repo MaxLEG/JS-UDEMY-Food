da94382687da4534593ef0cd910af270e956a7e8
Max <soldier322@gmail.com>
2021-01-05 20:10:34 +0100

    adding else/if for spectateur

--- script.js
+++ script.js
@@ -11,7 +11,19 @@ let personalMovieDB = {
 for (let i = 0; i < 2; i++) {
     const a = prompt("Один из  последних просмотренных фильмов?", ""),
         b = +prompt("На сколько оцените его?", "");
-    personalMovieDB.movies[a] = b;
-
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("done");
+    } else {
+        console.log("error");
+        i--;
+    }
 }
 
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов.");
+} else if (personalMovieDB.count >= 10 || personalMovieDB.count < 30) {
+    alert("Вы классический зритель.");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман!");
+}
